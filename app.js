05a8f4ee17750d6a179167a932d1c6d311d77da8
zando <zagodirenkomladshiy1117@gmail/com>
2024-04-10 12:05:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+const multiplyBtn = document.getElementById('multiply')
+const divideBtn = document.getElementById('divide')
+let action = '+'
+
+minusBtn.onclick = function () {
+  action = '-'
+}
+plusBtn.onclick = function () {
+  action = '+'
+}
+divideBtn.onclick = function () {
+  action = '/'
+}
+multiplyBtn.onclick = function () {
+  action = '*'
+}
+
+function printResult(result) {
+  if (result < 0) {
+    resultElement.style.color = 'red'
+  } else {
+    resultElement.style.color = 'green'
+  } 
+  resultElement.textContent = result
+}
+
+function computeNumbersWithActions (inp1, inp2, actionSymbol) {
+  const num1 = Number(inp1.value)
+  const num2 = Number(inp2.value)
+  if (actionSymbol == '+') {
+    return num1 + num2
+  } 
+  else if (actionSymbol == '-') {
+    return num1 - num2
+  } 
+  else if (actionSymbol == '*') {
+    return num1 * num2
+  }
+  else if (actionSymbol == '/') {
+    return num1 / num2
+  } 
+}
+
+submitBtn.onclick = function () {
+  const result = computeNumbersWithActions(input1, input2, action)
+  printResult(result)
+  // if (action == '+') {
+  //   const sum = Number(input1.value) + Number(input2.value)
+  //   printResult(sum)
+  // } else if (action == '-') {
+  //   const sum = Number(input1.value) - Number(input2.value)
+  //   printResult(sum)
+  // }
+}
